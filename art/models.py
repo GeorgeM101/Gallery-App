68734b669694d667f93ffce20852d0cb6625398c
GeorgeM101 <georgmboya@gmail.com>
2022-02-27 15:47:29 +0300

    created class for category

--- art/models.py
+++ art/models.py
@@ -6,3 +6,9 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+class Location(models.Model):
+    name = models.CharField(max_length = 20)
+
+    def __str__ (self):
+        return self.name
