5e435ebcf75195994a4d9dab2ae12704d7808c6c
GeorgeM101 <georgmboya@gmail.com>
2022-03-02 02:55:26 +0300

    added images

--- art/urls.py
+++ art/urls.py
@@ -9,5 +9,3 @@ urlpatterns=[
     path('location/<location>/', views.view_by_location, name='locationPage'), 
 ]
 
-if settings.DEBUG:
-    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
